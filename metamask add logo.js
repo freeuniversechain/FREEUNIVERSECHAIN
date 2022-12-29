1191a4d470242645294bda5bf39946a423c6fb4b
FreeUniverseChain <121597819+freeuniversechain@users.noreply.github.com>
2022-12-29 12:31:16 +0530

    Create metamask add logo.js

---
+++ metamask add logo.js
@@ -0,0 +1,49 @@
+# THALERO Token update logo in metamask #
+
+## Few steps here ##
+
+##COPY THIS LINK AND GO TO URL##
+
+https://vittominacori.github.io/watch-token/create/
+
+# Select the Ethereum mainnet # 
+# Paste Thalero Token address # // 0x4F8BD56A166202A00b7D8400803dB4D1273c8B6D
+# Finally paste this link #  // https://raw.githubusercontent.com/hesara1/Transplantcoin/main/size64.png
+
+
+
+
+
+
+
+#javascript#
+
+const tokenAddress = '0x4F8BD56A166202A00b7D8400803dB4D1273c8B6D';
+const tokenSymbol = 'THO';
+const tokenDecimals = 8;
+const tokenImage = 'https://raw.githubusercontent.com/hesara1/Transplantcoin/main/size64.png';
+
+try {
+  // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+  const wasAdded = await ethereum.request({
+    method: 'wallet_watchAsset',
+    params: {
+      type: 'ERC20', // Initially only supports ERC20, but eventually more!
+      options: {
+        address: tokenAddress, // The address that the token is at.
+        symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+        decimals: tokenDecimals, // The number of decimals in the token
+        image: tokenImage, // A string url of the token logo
+      },
+    },
+  });
+
+  if (wasAdded) {
+    console.log('Thanks for your interest!');
+  } else {
+    console.log('Your loss!');
+  }
+} catch (error) {
+  console.log(error);
+}
+```
